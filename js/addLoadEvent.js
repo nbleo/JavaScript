3d037f6b89444e0362386380038b7b6ffc47a58f
leo <nbleo.lee@qq.com>
2015-12-07 12:33:26 +0800

    Initial commit

---
+++ js/addLoadEvent.js
@@ -0,0 +1,17 @@
+//addLoadEvent 加载函数
+function addLoadEvent (func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != "function") {
+		window.onload = func;
+	} else {
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
+
+
+addLoadEvent(displayAbbreviations);
+addLoadEvent(displayCitations);
+addLoadEvent(displayAccesskeys);
---
+++ 收集JS脚本/addLoadEvent.js
@@ -0,0 +1,12 @@
+//addLoadEvent 加载函数
+function addLoadEvent (func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != "function") {
+		window.onload = func;
+	} else {
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
